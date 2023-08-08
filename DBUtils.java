fa871cc2aebece0dbd3f1da1df1cd3284ca71a48
Gowthami <rgowthamireddy6@gmail.com>
2023-08-08 19:13:52 +0530

    merge conflit resolved

--- DBUtils.java
+++ DBUtils.java
@@ -1,8 +1,8 @@
-
-hello..
+hello
 i am in master branch
 hello i am updating in dev branch
-hello i am updating in dev branch..
+hello i am updating in dev branch
 hello guys,
 git commands are very useful...
-hello...
+hello gowthami
+gud eveng
